4e8689fa31d0e22c9dd722741618109b43ae07d7
Sefa-Git <sefa@superapplabs.co>
2024-07-03 12:56:21 +0300

    try2 App.js

--- App.js
+++ App.js
@@ -26,6 +26,7 @@ export default function App() {
   function deleteGoalHandler(id){
   setCourseGoals(currentCourseGoals =>{
     consol.log('works')
+    consol.log('works2')
     return currentCourseGoals.filter((goal) => goal.id !== id);
   });
   }  
